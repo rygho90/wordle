68e24ed7d6fec30289e00ef68e6ae17dec8096bd
Ryan <rygho90@gmail.com>
2022-08-12 11:59:56 -0400

    Add gameController and displayController, add backspace functionality

--- script.js
+++ script.js
@@ -1,13 +1,121 @@
-const squares = document.querySelectorAll('[data-square]')
+const squareElements = document.querySelectorAll("[data-square]");
 
-const validLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const validLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-let activeSquare = 0
+const gameController = (() => {
+  const gameBoard = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ];
+  let activeSquare = 0;
 
+  const incrementActiveSquare = () => activeSquare++;
+  const decrementActiveSquare = () => activeSquare--;
 
-document.addEventListener('keypress', (e) => {
-    if (!validLetters.includes(e.key.toUpperCase())) return
-    squares[activeSquare].textContent = e.key.toUpperCase()
-    activeSquare++
-})
+  const getGameBoard = () => gameBoard;
+
+  const addLetter = (letter) => {
+    gameBoard[activeSquare] = letter;
+    incrementActiveSquare();
+  };
+
+  const deleteLetter = () => {
+    if (activeSquare === 0) return;
+    decrementActiveSquare();
+    gameBoard[activeSquare] = null;
+  };
+
+  const resetGameBoard = () => {
+    gameBoard.forEach((square) => {
+      gameBoard[square] = null;
+    });
+  };
+
+  return {
+    getGameBoard,
+    addLetter,
+    deleteLetter,
+    resetGameBoard,
+  };
+})();
+
+const displayController = (() => {
+  const renderBoard = (board) => {
+    let count = 0;
+    squareElements.forEach((square) => {
+      square.textContent = board[count];
+      count++;
+    });
+  };
+
+  return { renderBoard };
+})();
+
+document.addEventListener("keypress", (e) => {
+  if (!validLetters.includes(e.key.toUpperCase())) return;
+
+  gameController.addLetter(e.key.toUpperCase());
+  displayController.renderBoard(gameController.getGameBoard());
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Backspace") {
+    gameController.deleteLetter();
+    displayController.renderBoard(gameController.getGameBoard());
+  }
+});
